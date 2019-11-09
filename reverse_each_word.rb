479f60c5cc856bb606134955ee98e3d81911d9e4
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-09 01:05:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ end
 #def reverse_each_word(string)
 #  word_array = string.split(" ")
 #  reversed_array = word_array.collect do |word|
-#    word = word.reverse 
-#  end 
+#    word = word.reverse
+#  end
 #  return reversed_array.join(" ")
 #end
